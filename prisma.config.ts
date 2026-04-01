f2e0532879c4510ebd8b460f8c93763a195ad58f
Santi-ElSABROSO-Masias <101581953+Santi-ElSABROSO-Masias@users.noreply.github.com>
2026-04-01 18:52:15 -0500

    chore: add dns module and set default result order in prisma configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import dns from "dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,6 +11,5 @@ export default defineConfig({
   },
   datasource: {
     url: process.env["DATABASE_URL"],
-
   },
 });
